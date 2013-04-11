ff3665f27603ad0d4f765aa6f56288e1b1903459
francesquini <emilio.francesquini@imag.fr>
2013-04-11 10:44:27 +0200

    Moving src folder to the root directory Added clean script

---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -fr CMakeCache.txt cmake_install.cmake CMakeFiles Makefile 
+rm -fr x86/CMakeCache.txt x86/cmake_install.cmake x86/CMakeFiles x86/Makefile x86/bin
+rm -fr utils/CMakeCache.txt utils/cmake_install.cmake utils/CMakeFiles utils/Makefile utils/bin
