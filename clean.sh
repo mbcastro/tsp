721f52887fa85180bcc735f7c8adf555a252e424
francesquini <emilio.francesquini@imag.fr>
2013-04-11 11:02:20 +0200

    Added ARM Compilation

--- clean.sh
+++ clean.sh
@@ -2,4 +2,5 @@
 
 rm -fr CMakeCache.txt cmake_install.cmake CMakeFiles Makefile 
 rm -fr x86/CMakeCache.txt x86/cmake_install.cmake x86/CMakeFiles x86/Makefile x86/bin
+rm -fr arm/CMakeCache.txt arm/cmake_install.cmake arm/CMakeFiles arm/Makefile arm/bin
 rm -fr utils/CMakeCache.txt utils/cmake_install.cmake utils/CMakeFiles utils/Makefile utils/bin
