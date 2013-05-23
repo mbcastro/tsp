4a15bf493b37d0ff82bf2a865490d91ec3dd8e67
Emilio Francesquini <francesquini@uv2000.ujf-grenoble.fr>
2013-05-23 22:16:02 +0200

    Correcting time meausurement errors on 32bits architectures

--- src/timer.c
+++ src/timer.c
@@ -5,16 +5,18 @@
 static uint64_t residual_error = 0;
 
 void init_time(void) {
-	unsigned long t1, t2;
-	t1 = get_time();
-	t2 = get_time();
-	residual_error = t2 - t1;
+  uint64_t t1, t2;
+  t1 = get_time();
+  t2 = get_time();
+  residual_error = t2 - t1;
 }
 
 inline uint64_t get_time(void) {
-	struct timeval t1;
-	gettimeofday(&t1, NULL);
-	return 1000000L * t1.tv_sec + t1.tv_usec;
+  struct timeval t1;
+  gettimeofday(&t1, NULL);
+  uint64_t ret = UINT64_C(1000000) * ((uint64_t) t1.tv_sec);
+  ret += (uint64_t) t1.tv_usec;
+  return ret;
 }
 
 inline uint64_t diff_time(uint64_t t1, uint64_t t2) {
