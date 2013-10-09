e2e5e21a2e63f6614719d275cdca2d51918eb6e8
Emilio Francesquini <francesquini@gmail.com>
2013-10-09 15:19:20 +0200

    Updated code to be compatible with version 1.0 of the toolchain

--- mppa/run_mppa.sh
+++ mppa/run_mppa.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-#k1-jtag-runner --exec-file=IODDR0:$1 ${*:2} 
+# For the last parameter remember to add a leading 0 otherwise the
+# k1-jtag-runner will ignore the last parameter
+#
+# Ex, to run with two clusters:
+#
+# ./run_mppa.sh bin/tsp_lock_mppa.mpk tsp_lock_mppa_master 16 10 122 02
+
 k1-jtag-runner --multibinary=$1 --exec-multibin=IODDR0:$2 ${*:3}
